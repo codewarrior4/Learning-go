7ef2f3320639134a41eb8351487f17a9f8d163da
codewarrior4 <mayowatope119@gmail.com>
2024-04-22 05:49:47 -0700

    first commit

---
+++ hello.go
@@ -0,0 +1,26 @@
+package main
+
+import "fmt"
+
+// import "rsc.io/quote"
+
+func main() {
+    
+	var s string = "Hello, World!"
+	fmt.Println(s)
+
+	var b,c int = 1,2
+	fmt.Println(b,c)
+
+	var d,e bool = true,false
+	fmt.Println(d,e)
+
+	var f,g int32 = 1,2
+	fmt.Println(f,g)
+
+	var h,i int64 = 1907990988897,26879909908
+	fmt.Println(h,i)
+
+	var j,k uint = 1,2
+	fmt.Println(j,k)
+}
