1eaff7abe9f8cdee02efe28d6671f8379766db8c
codewarrior4 <mayowatope119@gmail.com>
2024-04-25 12:13:09 -0700

    maps

--- hello.go
+++ hello.go
@@ -2,67 +2,43 @@ package main
 
 import (
 	"fmt"
-	"slices"
+	"maps"
 )
 
 func main() {
+	
+	m:= make(map[string]int)
 
-	var s []string
-    fmt.Println("uninit:", s, s == nil, len(s) == 0)
-	// this print s which is an empty array, next s is empty, and len(s) is 0
+	m["k1"] = 7
+    m["k2"] = 13
 
-	s = make([]string, 3)
-	// s=  []string{"a", "b", "c"}
-    fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
+    fmt.Println("map:", m)
 
-	s[0] = "a"
-    s[1] = "b"
-    s[2] = "c"
-    fmt.Println("set:", s)
-    fmt.Println("get:", s[2]) 
+	v1 := m["k1"]
+    fmt.Println("v1:", v1)
 
-    fmt.Println("len:", len(s))
+    v3 := m["k3"]
+    fmt.Println("v3:", v3)
 
-	s = append(s, "d")
-    s = append(s, "e", "f")
-    fmt.Println("apd:", s)
+    fmt.Println("len:", len(m))
 
+	// deletes a key
+	delete(m, "k2"); 
+	fmt.Println("map:", m)
 
-	// you can use the slices.Clone function or copy function to copy a slice
-	c := slices.Clone(s)
-	// c := make([]string, len(s))
-    // copy(c, s)
-    fmt.Println("cpy:", c)
+	// deletes all keys
+	clear(m)
+	fmt.Println("map:", m)
 
-	// prints between index 2 and 5 skpping index 5
-	l := s[2:5]
-    fmt.Println("sl1:", l)
+	_, prs := m["k2"]
+    fmt.Println("prs:", prs)
 
-	// prints from 0 to 5 but skips index 5
-	l = s[:5]
-    fmt.Println("sl2:", l)
+	n := map[string]int{"foo": 1, "bar": 2}
+    fmt.Println("map:", n)
 
-	// prints from 2 to the end
-	l = s[2:]
-    fmt.Println("sl3:", l)
-
-	t := []string{"g", "h", "i"}
-    fmt.Println("dcl:", t)
-
-    t2 := []string{"g", "h", "i"}
-    if slices.Equal(t, t2) {
-        fmt.Println("t == t2")
+    n2 := map[string]int{"foo": 1, "bar": 2}
+    if maps.Equal(n, n2) {
+        fmt.Println("n == n2")
     }
-
-	twoD := make([][]int, 4)
-    for i := 0; i < 4; i++ {
-        innerLen := i + 1
-        twoD[i] = make([]int, innerLen)
-        for j := 0; j < innerLen; j++ {
-            twoD[i][j] = i + j
-        }
-    }
-    fmt.Println("2d: ", twoD)
-
-
+	
 }
