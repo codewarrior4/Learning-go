7516f0dcfa0a0b1c365e3a2c213bb14d774885ec
codewarrior4 <mayowatope119@gmail.com>
2024-04-25 11:28:50 -0700

    slices

--- hello.go
+++ hello.go
@@ -1,41 +1,68 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"slices"
+)
 
 func main() {
 
-    var a [5]int
-    fmt.Println("emp:", a)
+	var s []string
+    fmt.Println("uninit:", s, s == nil, len(s) == 0)
+	// this print s which is an empty array, next s is empty, and len(s) is 0
 
-    a[4] = 100
-    fmt.Println("set:", a)
-    fmt.Println("get:", a[4])
+	s = make([]string, 3)
+	// s=  []string{"a", "b", "c"}
+    fmt.Println("emp:", s, "len:", len(s), "cap:", cap(s))
 
-    fmt.Println("len:", len(a))
+	s[0] = "a"
+    s[1] = "b"
+    s[2] = "c"
+    fmt.Println("set:", s)
+    fmt.Println("get:", s[2]) 
 
-	// intialize array in one line
-    b := [5]int{1, 2, 3, 4, 5}
-    fmt.Println("dcl:", b)
+    fmt.Println("len:", len(s))
 
-	// intialize array with compiler count
-    b = [...]int{1, 2, 3, 4, 5}
-    fmt.Println("dcl:", b)
+	s = append(s, "d")
+    s = append(s, "e", "f")
+    fmt.Println("apd:", s)
 
-	
-    b = [...]int{100, 3: 400, 500}
-    fmt.Println("idx:", b)
 
-    var twoD [2][3]int
-    for i := 0; i < 2; i++ {
-        for j := 0; j < 3; j++ {
+	// you can use the slices.Clone function or copy function to copy a slice
+	c := slices.Clone(s)
+	// c := make([]string, len(s))
+    // copy(c, s)
+    fmt.Println("cpy:", c)
+
+	// prints between index 2 and 5 skpping index 5
+	l := s[2:5]
+    fmt.Println("sl1:", l)
+
+	// prints from 0 to 5 but skips index 5
+	l = s[:5]
+    fmt.Println("sl2:", l)
+
+	// prints from 2 to the end
+	l = s[2:]
+    fmt.Println("sl3:", l)
+
+	t := []string{"g", "h", "i"}
+    fmt.Println("dcl:", t)
+
+    t2 := []string{"g", "h", "i"}
+    if slices.Equal(t, t2) {
+        fmt.Println("t == t2")
+    }
+
+	twoD := make([][]int, 4)
+    for i := 0; i < 4; i++ {
+        innerLen := i + 1
+        twoD[i] = make([]int, innerLen)
+        for j := 0; j < innerLen; j++ {
             twoD[i][j] = i + j
         }
     }
     fmt.Println("2d: ", twoD)
 
-    twoD = [2][3]int{
-        {1, 2, 3},
-        {1, 2, 3},
-    }
-    fmt.Println("2d: ", twoD)
+
 }
