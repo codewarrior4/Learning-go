5154a3116b02c4f946cf86d95aae7a1b7db0b195
codewarrior4 <mayowatope119@gmail.com>
2024-05-07 03:19:03 -0700

    recursions

--- hello.go
+++ hello.go
@@ -25,8 +25,18 @@ func sum(nums ...int) {
 	fmt.Println(total)
 }
 
+// recursions
+
+func fact(n int) int {
+	if n == 0 {
+		return 1
+	}
+	return n * fact(n - 1)
+}
+
 func main(){
 
+	fmt.Println(fact(7))
 	fmt.Println(addition(1, 2))
 	a, b := vals()
 	fmt.Println(a, b)
